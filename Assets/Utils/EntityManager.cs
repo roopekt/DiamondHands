634b2a2b09630207f11e44755af640bec8fc90a1
Yupsilon <paul.sendroiu@gmail.com>
2025-01-24 21:44:01 +0200

    Entman

--- Assets/Utils/EntityManager.cs
+++ Assets/Utils/EntityManager.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityManager : MonoBehaviour
 {
     public static EntityManager instance;
+    List<EntityBase> entites = new();
     private void Awake()
     {
         if (instance == null)
@@ -12,6 +14,33 @@ public class EntityManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+    public EntityBase SpawnFromPrefab(GameObject entityPrefab)
+    {
+        GameObject entityParent = GameManager.instance.entityPool.PoolItem(entityPrefab);
+        if (entityParent?.TryGetComponent(out EntityBase entity) ?? false)
+        {
+            Spawn(entity);
+            return entity;
+        }
+        return null;
+    }
+    public void Spawn(EntityBase entity)
+    {
+        if (!entites.Contains(entity))
+        {
+            entity.Spawn();
+            entites.Add(entity);
+        }
+    }
+
+    public void Despawn(EntityBase entity) { 
+        if (entites.Contains(entity))
+        {
+            entity.Despawn();
+            entites.Remove(entity);
+            GameManager.instance.entityPool.DeactivateObject(entity.game);
         }
     }
 }
