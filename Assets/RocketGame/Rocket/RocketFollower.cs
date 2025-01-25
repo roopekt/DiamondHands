b8c4f6e43375af6346ed0c9a22a62e432fa97de1
Roope Keto-Tokoi <roope.keto-tokoi@outlook.com>
2025-01-25 22:14:00 +0200

    3D rocket

--- Assets/RocketGame/Rocket/RocketFollower.cs
+++ Assets/RocketGame/Rocket/RocketFollower.cs
@@ -9,6 +9,10 @@ public class RocketFollower : MonoBehaviour
     {
         transform.position = rocketParent.transform.position;
         if (rocketParent.isActive)
-        transform.up = new Vector3(rocketParent.constantXSpeed, rocketParent.GetVelY(), 0);
+
+        transform.rotation =
+            Quaternion.FromToRotation(Vector3.up, new Vector3(rocketParent.constantXSpeed, rocketParent.GetVelY(), 0))
+            * Quaternion.AngleAxis(-90f, Vector3.right);
+        
     }
 }
