0aed30cbb5f1be6c1a192900336499018a3ab7a7
Yupsilon <paul.sendroiu@gmail.com>
2025-01-25 12:10:44 +0200

    Fish final

--- Assets/Fish/FishCon.cs
+++ Assets/Fish/FishCon.cs
@@ -1,34 +1,31 @@
 using UnityEngine;
 
 public class FishCon : MonoBehaviour
-{ 
+{
     public float maximumRotationSpeed = 3;
     public float maximumVelocity = 3;
 
-    public float a = 1;
-    public float b = .35f;
+    public float sprintAngle = 1;
+    public float turnSpeed = .35f;
 
-    public float lerpedAngle;
-    public float deltaAngle;
-    
     public Rigidbody2D rigidbody;
     Vector2 moveDirection;
 
     private void Update()
     {
-        moveDirection = transform.position -  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        moveDirection =  Camera.main.ScreenToWorldPoint(Input.mousePosition)- transform.position;
     }
     public void FixedUpdate()
     {
-         lerpedAngle = (Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg) % 360;
-                float absoluteMaxRot = maximumRotationSpeed ;
+        float lerpedAngle = (Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg) % 360;
+        float absoluteMaxRot = maximumRotationSpeed;
 
-         deltaAngle = Mathf.DeltaAngle( lerpedAngle, rigidbody.rotation);
+        float deltaAngle = Mathf.DeltaAngle( rigidbody.rotation, lerpedAngle);
 
         deltaAngle = Mathf.Clamp(deltaAngle, -absoluteMaxRot, absoluteMaxRot);
 
-        float velMult = Mathf.Abs(deltaAngle) > a ? 1 : b;
+        float velMult = Mathf.Abs(deltaAngle) < sprintAngle ? 1 : turnSpeed;
         rigidbody.linearVelocity = transform.right * velMult * maximumVelocity;
-        rigidbody.angularVelocity = deltaAngle;
+        rigidbody.rotation += deltaAngle;
     }
 }
